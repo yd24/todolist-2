f617dde56bfce93a35729140f784a32fb478ac43
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2024-02-14 22:24:24 -0800

    Add boilerplate for DB connection through PG and routing

--- server/app.js
+++ server/app.js
@@ -7,11 +7,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//initialize pool of connections to PostgreSQL DB
+const Pool = require('pg').Pool;
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DB,
+  password: process.env.PW,
+  port: process.env.DB_PORT,
+});
+
+//testing server response
+app.get('/todo', (req, res) => {
+  res.send('You got the todo!');
+});
+
+app.get('*', (req, res) => {
+  res.send('No resource here.');
+});
+
 module.exports = {
   app,
   start: (PORT) => {
     app.listen(PORT, () => {
-      console.log(`Listening on port {PORT}.`);
+      console.log(`Listening on port ${PORT}.`);
     });
   }
 }
