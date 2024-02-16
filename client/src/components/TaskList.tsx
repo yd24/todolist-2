457f20b439f84a64d7d56ed43172e287fb4867a2
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2024-02-15 19:58:40 -0800

    Fetch and display tasks

--- client/src/components/TaskList.tsx
+++ client/src/components/TaskList.tsx
@@ -1,13 +1,50 @@
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+
 export function TaskList() {
+  interface Task {
+    taskID: number,
+    title: string,
+    content: string,
+    is_completed: boolean,
+    point_value: number,
+    created_at: string,
+  }
+
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const getTasks = async() => {
+    try {
+      const config = {
+        method: 'get',
+        baseURL: import.meta.env.VITE_REACT_APP_SERVER,
+        url: '/task',
+      };
+      const results = await axios<Task[]>(config);
+      setTasks(results.data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
   return (
     <div id="tasklist">
       <ul>
         {
-          [...Array(10)].map((ele, i) => (
-            <li key={i}>
-              Buy groceries
-            </li>
-          ))
+          tasks.map((task: Task, idx) => {
+            return (
+              <li key={idx}>
+                <h3>{task.title}</h3>
+                <p>{task.content}</p>
+                <p>Points: {task.point_value}</p>
+                <p>Created on: {new Date(task.created_at).toDateString()}</p>
+              </li>
+            );
+          })
         }
       </ul>
     </div>
