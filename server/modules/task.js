f2ab6cabe2928b864cfc24571bfe76cc28a7f89a
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2024-02-16 21:02:21 -0800

    Completed basic CRUD functions for task

--- server/modules/task.js
+++ server/modules/task.js
@@ -8,7 +8,7 @@ async function getAllTasks(req, res, next) {
   try {
     const query = {
       text: 'SELECT * FROM task ORDER BY created_at DESC',
-    }
+    };
     const allTasks = await pool.query(query);
     res.json(allTasks.rows);
   } catch (err) {
@@ -17,29 +17,68 @@ async function getAllTasks(req, res, next) {
 };
 
 async function getSingleTask(req, res, next) {
-  res.send('Got a single task.');
+  try {
+    const query = {
+      text: 'SELECT * FROM task WHERE $1 = taskID',
+      values: [req.params.id],
+    };
+    const singleTask = await pool.query(query);
+    res.json(singleTask.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 async function createTask(req, res, next) {
-  const {title, content, point_value} = req.body;
-  const query = {
-    text: 'INSERT INTO task(title, content, point_value) VALUES($1, $2, $3) RETURNING *',
-    values: [
-      title,
-      content,
-      point_value,
-    ],
+  try {
+    const {title, content, point_value} = req.body;
+    const query = {
+      text: 'INSERT INTO task(title, content, point_value) VALUES($1, $2, $3) RETURNING *',
+      values: [
+        title,
+        content,
+        point_value,
+      ],
+    }
+    const newTask = await pool.query(query);
+    res.json(newTask.rows[0]);
+  } catch (err) {
+    console.error(err);
   }
-  const newTask = await pool.query(query);
-  res.json(newTask.rows[0]);
 };
 
 async function updateTask(req, res, next) {
-  res.send('Updated a task.');
+  try {
+    const {title, content, point_value} = req.body;
+    const query = {
+      text: 'UPDATE task SET title = $1, content = $2, point_value = $3 WHERE taskID = $4 RETURNING *',
+      values: [
+        title,
+        content,
+        point_value,
+        req.params.id,
+      ],
+    };
+    const updatedTask = await pool.query(query);
+    res.json(updatedTask.rows[0]);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
+//add function that completes task and adds points to rewards.
+
 async function deleteTask(req, res, next) {
-  res.send('Deleted a task.');
+  try {
+    const query = {
+      text: 'DELETE FROM task WHERE taskID = $1 RETURNING *',
+      values: [req.params.id],
+    };
+    const updatedTask = await pool.query(query);
+    res.json(updatedTask.rows[0]);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports = {
