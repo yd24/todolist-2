55651d9213250a1e509bee9cd088ea4713ca63e9
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2024-02-19 21:27:37 -0800

    Add routes and API methods for rewards and point tracking

--- server/app.js
+++ server/app.js
@@ -7,13 +7,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//Methods for Task//
 const taskMethods = require('./modules/task');
+const rewardMethods = require('./modules/reward');
+const pointMethods = require('./modules/points');
+
+//Task Routes//
 app.get('/task', taskMethods.getAllTasks);
 app.get('/task/:id', taskMethods.getSingleTask);
 app.post('/task', taskMethods.createTask);
 app.put('/task/:id', taskMethods.updateTask);
 app.delete('/task/:id', taskMethods.deleteTask);
+
+//Reward Routes//
+app.get('/reward', rewardMethods.getAllRewards);
+app.get('/reward/:id', rewardMethods.getReward);
+app.post('/reward', rewardMethods.createReward);
+app.put('/reward/:id', rewardMethods.updateReward);
+app.delete('/reward/:id', rewardMethods.deleteReward);
+
+//Routes for tracking points//
+app.delete('/points', pointMethods.removePointsToReward);
+app.post('/points', pointMethods.addPointsToReward);
+app.get('/points/:id', pointMethods.getCurrentPoints);
 
 //Test server response
 app.get('*', (req, res, next) => {
